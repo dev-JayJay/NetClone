c17b551e62663ae16c1162c8f570819e1de474ae
dev-jay-jay <dev-jay-jay>
2023-11-23 14:04:48 -0800

    check 1

--- src/component/Fetch.js
+++ src/component/Fetch.js
@@ -1,41 +1,37 @@
 import { useEffect, useState } from "react";
 
+const Fetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const Fetch = (url) => {
-      const [data, setData] = useState(null);
-      const [loading, setLoading] = useState(true);
-      const [error, setError] = useState(null);
+  useEffect(() => { 
+    const abortCont = new AbortController();
 
-      useEffect(() => { 
-        const abortCont = new AbortController();
- 
-        fetch(url, { signal: abortCont.signal })
-        .then((response) => {
-          if(!response.ok){
-            // console.log('checking sever') tring checking response with data when it is ok
-            throw Error("PLease Reload Page There's An Error Fetching API");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setData(data);
+    fetch(url.replace("http://", "https://"), { signal: abortCont.signal })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Please Reload Page, There's An Error Fetching API");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          console.log('No fetch');
+        } else {
+          setError(error.message);
           setLoading(false);
-        })
-        .catch((error) => {
-          if(error.name === 'AbortError'){
-            console.log('no fetch')
-          }else {
-            setError(error.message);
-            setLoading(false);
-          }
-        })
+        }
+      });
 
-        return () => abortCont.abort();
+    return () => abortCont.abort();
+  }, [url]);
 
-      },[url])
+  return { data, loading, error };
+};
 
-      return { data , loading, error }
-
-    }
-
-    export default Fetch;
+export default Fetch;
